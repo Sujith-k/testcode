835121b8ee6ee937b72f93a1abaa90dc5d93745d
sujith <sujithnair1985@gmail.com>
2020-04-06 17:58:13 +0530

    Updates

--- src/api/api.js
+++
@@ -1,7 +0,0 @@
-import itemList from "../components/itemList"
-
-export const getVideosApi = (pageNumber) => {
-    return fetch(`/assets/data/CONTENTLISTINGPAGE-PAGE${pageNumber}.json`)
-        .then(data => data.json())
-        .then( data => data.page['content-items'].content) //Assuming we have a solid contract with API for the structure.
-}
--- src/constants/constants.js
+++
@@ -1,3 +0,0 @@
-export const GET_VIDEOS = 'GET_VIDEOS';
-export const RECIEVED_VIDEOS = 'RECIEVED_VIDEOS';
-export const DO_SEARCH = 'DO_SEARCH';
